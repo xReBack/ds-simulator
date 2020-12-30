f5201b50d46f35d4240060c817f305408ec5fb91
BakaarCF <73856296+BakaarCF@users.noreply.github.com>
2020-12-30 22:18:47 +0200

    code updated

--- DS-Simulator Phase 2/DS-Simulator Phase 2/DS-Simulator Phase 2.cpp
+++ DS-Simulator Phase 2/DS-Simulator Phase 2/DS-Simulator Phase 2.cpp
@@ -1,6 +1,3 @@
-// ConsoleApplication18.cpp : This file contains the 'main' function. Program execution begins and ends there.
-//
-
 #include <cmath>
 #include <iostream>
 #include <algorithm>
@@ -9,9 +6,7 @@
 #include <string>
 using namespace std;
 
-//*********************************************************************************
 //********************************** FCFS *****************************************
-//*********************************************************************************
 void FCFS(int requests[], int head_position, int req_count, int distance, int seek_count)
 {
 	cout << "FCFS: " << endl;
@@ -31,10 +26,7 @@ void FCFS(int requests[], int head_position, int req_count, int distance, int se
 	cout << endl << endl;
 }
 
-
-//*********************************************************************************
 //********************************** SCAN *****************************************
-//*********************************************************************************
 void SCAN(int requests[], int head_position, int distance, int req_count, int seek_count, int disk_size, bool direction_right) {
 	cout << "Scan: " << endl;
 	sort(requests, requests + req_count);
@@ -84,215 +76,304 @@ void SCAN(int requests[], int head_position, int distance, int req_count, int se
 	cout << endl << endl;
 }
 
-void CLOOK(int requests[], int head_position, int req_count, int distance, int seek_count)
-{
-	cout << "CLOOK: " << endl;
-
-	int old_head = head_position;
-	for (int i = 0; i < req_count; i++)
-	{
-		if (head_position < requests[i])
-		{
-			distance += abs(head_position - requests[i]);
-			head_position = requests[i];
-		}
-
-	}
-	distance += abs(head_position - requests[0]);
-	for (int i = 0; i < req_count; i++)
-	{
-		if (requests[i] < old_head && requests[i + 1] < old_head)
-
-			distance += abs(requests[i] - requests[i + 1]);
-	}
-	cout << "Order: " << old_head << " -> ";
+//********************************** CLOOK ****************************************
+void CLOOK(int requests[], int head_position, int req_count, int seek_count, int disk_size, bool direction_right) {
+	int* requests_x = new int[req_count];
 	for (int i = 0; i < req_count; i++) {
-		string _ = " -> ";
-		if (i == req_count - 1) {
-			_ = "";
-		}
-		cout << requests[i] << _;
+		requests_x[i] = requests[i];
 	}
-	cout << "\n" << "Seek: " << distance << " cylinders, " << distance * seek_count << "ms";
+	cout << "C-Look: " << endl;
+	sort(requests_x, requests_x + req_count);
+
+	list<int>leftt;
+	list<int>rightt;
+	for (int i = 0; i < req_count; i++)
+	{
+		if (requests_x[i] > head_position)
+			rightt.push_back(requests_x[i]);
+		else
+			leftt.push_back(requests_x[i]);
+	}
+	list<int> CLOOK_requests;
+	CLOOK_requests.push_back(head_position);
+	if (direction_right) {
+		leftt.reverse();
+	}
+	else {
+		rightt.reverse();
+	}
+	for (int i = 0; i < 2; i++)
+	{
+
+		if (direction_right)
+		{
+			for (list <int> ::iterator i = rightt.begin(); i != (rightt.end()); i++)
+			{
+				CLOOK_requests.push_back(*i);
+			}
+		}
+		else
+
+		{
+			for (auto i = leftt.rbegin(); i != (leftt.rend()); i++)
+			{
+
+				CLOOK_requests.push_back(*i);
+
+			}
+
+		}
+		if (i == 0)
+			//as not to walk till the end of the disk
+		/*{
+			LOOK_requests.push_back((end - 1)*upper);
+		}*/
+			direction_right = !direction_right;
+	}
+	int total_distance = 0;
+	cout << "Order: ";
+
+	for (list <int> ::iterator i = CLOOK_requests.begin(); i != prev(CLOOK_requests.end()); i++) {
+		cout << *i << " -> ";
+		//cout << *i << " - " << *next(i) << " = " << abs(*i - *next(i)) << endl;
+		total_distance += abs(*i - *next(i));
+
+	}
+	cout << *prev(CLOOK_requests.end());
+	cout << "\n" << "Seek: " << total_distance << " cylinders, " << total_distance * seek_count << "ms";
 	cout << endl << endl;
 }
 
-
-void CSCAN(int requests[], int head_position, int req_count, int distance, int seek_count)
+//********************************** CSCAN ****************************************
+void CSCAN(int requests[], int head_position, int req_count, int distance, int seek_count, int disk_size)
 {
 	cout << "CSCAN: " << endl;
-
-	for (int i = 0; i < req_count; i++)
-	{
-		if (head_position < requests[i])
-		{
-			distance += abs(head_position - requests[i]);
-			head_position = requests[i];
-		}
-
-	}
-
-	for (int i = 0; i < req_count; i++)
-	{
-		if (requests[i] < head_position && requests[i + 1] < head_position)
-
-			distance += abs(requests[i] - requests[i + 1]);
-	}
+	string _ = " -> ";
 	cout << "Order: " << head_position << " -> ";
+
+	sort(requests, requests + req_count);
+	distance = 0;
+	int bound_z = disk_size - 1;
+	bool bbound = false;
+	int next_index = 0;
 	for (int i = 0; i < req_count; i++) {
-		string _ = " -> ";
-		if (i == req_count - 1) {
-			_ = "";
+
+		if (requests[i] >= head_position && distance == 0)
+		{
+			distance = abs(head_position - requests[i]);
 		}
-		cout << requests[i] << _;
 	}
+
+	for (int i = 0; i < req_count; i++) {
+
+		if (requests[i] > head_position)
+		{
+
+			cout << requests[i] << _;
+			if (requests[i] == bound_z)
+			{
+				bbound = true;
+			}
+		}
+
+	}
+	for (int i = 0; i < req_count - 1; i++) {
+		if (requests[i] > head_position)
+		{
+			next_index = requests[i + 1];
+			distance += abs(requests[i] - next_index);
+		}
+
+	}
+	int bound_x = 0;
+
+	if (!bbound)
+	{
+
+		cout << bound_z << _;
+		distance += abs(bound_z - next_index);
+		distance += abs(bound_x - bound_z);
+		next_index = bound_x;
+
+
+	}
+	cout << bound_x << _;
+
+	for (int i = 0; i < req_count; i++)
+	{
+
+		if (requests[i] < head_position)
+		{
+
+			if (requests[i + 1] > head_position)
+			{
+				_ = "";
+			}
+			if (requests[i] != bound_x)
+			{
+				cout << requests[i] << _;
+
+			}
+
+
+
+		}
+	}
+
+	for (int i = 0; i < req_count; i++)
+	{
+		if (requests[i] < head_position)
+		{
+			distance += abs(requests[i] - next_index);
+
+		}
+		next_index = requests[i];
+
+	}
+
+
+
+
+
 	cout << "\n" << "Seek: " << distance << " cylinders, " << distance * seek_count << "ms";
 	cout << endl << endl;
 }
 
-void SSTF(int requests[], int head_position, int req_count, int distance, int seek_count, int disk_size)
-{
+//********************************** LOOK ****************************************
+void LOOK(int requests[], int head_position, int req_count, int seek_count, int disk_size, bool direction_right) {
+	int* requests_x = new int[req_count];
+	for (int i = 0; i < req_count; i++) {
+		requests_x[i] = requests[i];
+	}
+	cout << "LOOK: " << endl;
+	sort(requests_x, requests_x + req_count);
 
-
-	cout << "SSTF: " << endl;
-	int closest_index = disk_size - head_position;
-	int closest_i = disk_size - head_position;
-	//cout << "closest index = " << closest_index << endl;
-	for (int i = 0; i < req_count - 1; i++)
+	list<int>left;
+	list<int>right;
+	for (int i = 0; i < req_count; i++)
 	{
-
-		//cout << "distance = " << head_position << " - " << requests[i] << " = ";
-		distance = (head_position - requests[i]);
-		//cout << distance << endl;
-
-		if (closest_i > abs(distance)) {
-			closest_i = abs(distance);
-
-			closest_index = distance;
+		if (requests_x[i] > head_position)
+			right.push_back(requests_x[i]);
+		else
+			left.push_back(requests_x[i]);
+	}
+	list<int> LOOK_requests;
+	LOOK_requests.push_back(head_position);
+	for (int i = 0; i < 2; i++)
+	{
+		if (direction_right)
+		{
+			for (list <int> ::iterator i = right.begin(); i != (right.end()); i++)
+			{
+				LOOK_requests.push_back(*i);
+			}
 
 		}
-		//cout <<"closest index = " << closest_index << endl;
-		//cout << "closest i = " << closest_i << endl;
+		else
+		{
+			for (auto i = left.rbegin(); i != (left.rend()); i++)
+			{
+				LOOK_requests.push_back(*i);
+			}
+		}
+		if (i == 0)
+			direction_right = !direction_right;
+	}
+
+
+	int total_distance = 0;
+	cout << "Order: ";
+
+	for (list <int> ::iterator i = LOOK_requests.begin(); i != prev(LOOK_requests.end()); i++) {
+		cout << *i << " -> ";
+		total_distance += abs(*i - *next(i));
 
 	}
+	cout << *prev(LOOK_requests.end());
+	cout << "\n" << "Seek: " << total_distance << " cylinders, " << total_distance * seek_count << "ms";
+	cout << endl << endl;
+}
+
+//********************************** SSTF ****************************************
+void SSTF(int requests[], int head_position, int req_count, int seek_count, int disk_size) {
+	cout << "SSTF: " << endl;
+
+	int* requests_x = new int[req_count];
+	for (int i = 0; i < req_count; i++) {
+		requests_x[i] = requests[i];
+	}
+
+	int* sstf_requests = new int[req_count];
+	for (int i = 0; i < req_count; i++) {
+		sstf_requests[i] = requests_x[i];
+	}
+	int current_request = head_position;
+	int current_request_index = -1;
+	int least_distance = 99999;
+	int least_distance_index = 0;
+	for (int i = 0; i < req_count; i++) {
+		for (int j = 0; j < req_count; j++) {
+			if (j == current_request_index) {
+				continue;
+			}
+			if (least_distance > abs(current_request - requests_x[j])) {
+				least_distance = abs(current_request - requests_x[j]);
+				least_distance_index = j;
+			}
+		}
+		least_distance = 99999;
+		sstf_requests[i] = requests_x[least_distance_index];
+		current_request = requests_x[least_distance_index];
+		requests_x[least_distance_index] = 99999;
+	}
+
 	cout << "Order: " << head_position << " -> ";
 	string _ = " -> ";
-	int requests_x[8];
 
-	if (closest_index > 0) //Move left first
-	{
-		sort(requests, requests + req_count, greater<int>());
-		for (int i = 0; i < req_count; i++) {
-
-			if (requests[i] < head_position)
-			{
-
-				requests_x[i] = requests[i];
-				cout << requests_x[i] << _;
-				distance += abs(requests[i] - requests[i + 1]);
-
-			}
-
+	for (int i = 0; i < req_count; i++) {
+		string arrow = " -> ";
+		if (i == req_count - 1) {
+			arrow = "";
 		}
-
-
-		for (int i = 0; i < req_count; i++)
-		{
-			sort(requests, requests + req_count);
-
-			if (requests[i] > head_position)
-			{
-				requests_x[i] = requests[i];
-				if (i == req_count - 1)
-				{
-					_ = "";
-				}
-				cout << requests_x[i] << _;
-				distance += abs(requests[i] - requests[i + 1]);
-
-
-			}
-		}
+		cout << sstf_requests[i] << arrow;
 	}
 
-	else if (closest_index < 0) //Move left first
-	{
-
-		for (int i = 0; i < req_count; i++) {
-			sort(requests, requests + req_count, greater<int>());
-
-			if (requests[i] < head_position)
-			{
-
-				requests_x[i] = requests[i];
-				cout << requests_x[i] << _;
-				distance += abs(requests[i] - requests[i + 1]);
-
-			}
-		}
-
-		sort(requests, requests + req_count);
-
-		for (int i = 0; i < req_count; i++) {
-			if (requests[i] > head_position)
-			{
-				requests_x[i] = requests[i];
-				if (i == req_count - 1) {
-					_ = "";
-				}
-				cout << requests_x[i] << _;
-				distance += abs(requests[i] - requests[i + 1]);
-
-
-			}
-		}
-	}
-
-
-	distance = abs(head_position - requests[0]);
+	int total_distance = abs(head_position - sstf_requests[0]);
 	for (int i = 0; i < req_count - 1; i++) {
-		distance += abs(requests[i] - requests[i + 1]);
+		total_distance += abs(sstf_requests[i] - sstf_requests[i + 1]);
 	}
-
-
-	cout << "\n" << "Seek: " << distance << " cylinders, " << distance * seek_count << "ms";
+	cout << "\n" << "Seek: " << total_distance << " cylinders, " << total_distance * seek_count << "ms";
 	cout << endl << endl;
 }
 
-
-//*********************************************************************************
-//********************************* Driver Code ***********************************
-//*********************************************************************************
+//********************************** Driver Code **********************************
 int main()
 {
 	//INPUTS
-	int head_position = 50;
-	int disk_size = 200;
-	/*int* requests = NULL;
 	int n;
-	cout << "How many requests? : ";
+	/*cout << "How many requests? : ";
 	cin >> n;
-	cout << "Enter the list of requests"<<endl;
-	requests = new int[8];
-	for (int i = 0; i < 8; i++) {
+	cout << "enter the list of requests"<<endl;
+	int*requests = new int[n];
+	for (int i = 0; i < n; i++) {
 		cout << i+1 << " - ";
 		cin >> requests[i];
-	}*/
+	}
+	int req_count = n;*/
+	/**/ int requests[8] = { 98, 183, 40, 122, 10, 124, 65, 175 };
+	int req_count = sizeof(requests) / sizeof(requests[0]); /**/
+	int head_position = 50;
+	int disk_size = 200;
 	int seek_count = 6;
-	int requests[8] = { 176, 79, 34, 60, 92, 11, 41, 114 };
-	int req_count = sizeof(requests) / sizeof(requests[0]);
 	bool direction_right = false;
-	cout << "req count = " << req_count << endl;
 	int distance = 0;
 
+	LOOK(requests, head_position, req_count, seek_count, disk_size, direction_right);
+	SSTF(requests, head_position, req_count, seek_count, disk_size);
+	CSCAN(requests, head_position, req_count, distance, seek_count, disk_size);
+	CLOOK(requests, head_position, req_count, seek_count, disk_size, direction_right);
 	FCFS(requests, head_position, req_count, distance, seek_count);
-	SSTF(requests, head_position, req_count, distance, seek_count, disk_size);
-
 	SCAN(requests, head_position, distance, req_count, seek_count, disk_size, direction_right);
-	CLOOK(requests, head_position, req_count, distance, seek_count);
-	CSCAN(requests, head_position, req_count, distance, seek_count);
-
-
 }
 
 
